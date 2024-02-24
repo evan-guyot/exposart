df79a4e1d8d69c60a9d2e647dce521f0b9171d4f
Evan Guyot <evanguyot.pro@gmail.com>
2024-02-24 19:48:46 +0100

    fix:changes-of-styles

--- src/app/masterpieces/[id]/page.tsx
+++ src/app/masterpieces/[id]/page.tsx
@@ -5,17 +5,17 @@ export default async function Page({ params }: { params: { id: string } }) {
   const masterpiece = await getMasterpiece(params.id);
 
   return (
-    <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 m-auto">
+    <div className="relative flex flex-col mt-6 text-white bg-gray-800 shadow-md bg-clip-border rounded-xl w-96 m-auto">
       <div
-        className="cs-masonry-image relative h-96 mt-6 mx-4 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+        className="cs-masonry-image relative h-96 mt-6 mx-4 overflow-hidden shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
         style={{
           backgroundImage: `url(${masterpiece.url})`,
         }}
       ></div>
       <div className="p-6">
-        <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+        <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal">
           {masterpiece.name}
-          <span className="text-sm font-sans font-medium leading-normal text-blue-gray-400 float-right">
+          <span className="text-sm font-sans font-medium leading-normal float-right">
             {masterpiece.price}e
           </span>
         </h5>
@@ -25,12 +25,12 @@ export default async function Page({ params }: { params: { id: string } }) {
       </div>
       <div className="p-6 pt-0 flex flex-row justify-evenly">
         <CartAddButton
-          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-black text-white shadow-md hover:bg-gray-950 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none cursor-pointer"
           label="Add to Cart"
           masterpiece={masterpiece}
         />
         <button
-          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-black text-white shadow-md hover:bg-gray-950 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none cursor-pointer"
           type="button"
         >
           Copy Link
--- src/app/masterpieces/page.tsx
+++ src/app/masterpieces/page.tsx
@@ -13,17 +13,18 @@ export default async function Page() {
               height: m.height,
               backgroundImage: `url(${m.url})`,
             }}
-            className="cs-masonry-image mb-6 hover:shadow-2xl rounded-xl"
+            className="cs-masonry-image cs-mansory-image-hover mb-6 hover:shadow-2xl rounded-xl"
           >
             <div
               style={{
+                display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
                 height: "100%",
                 padding: "1rem",
                 breakInside: "avoid-column",
               }}
-              className="rounded-xl flex"
+              className="rounded-xl hidden"
             >
               <h3
                 style={{
@@ -55,7 +56,7 @@ export default async function Page() {
                   masterpiece={m}
                   style={{ bottom: 10, width: "fit-content" }}
                   label="Add to cart"
-                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 cursor-pointer"
                 />
               </div>
             </div>
